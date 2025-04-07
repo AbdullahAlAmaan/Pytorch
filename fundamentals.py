7f090d38b808a39effe10ad9e58ced128c22e83d
Abdullah Al Amaan <126609300+AbdullahAlAmaan@users.noreply.github.com>
2025-04-07 17:07:09 -0400

    .

--- fundamentals.py
+++ fundamentals.py
@@ -14,3 +14,13 @@ matrix=torch.tensor([[1,2],[3,4]])
 print(matrix)
 print(matrix.ndim)
 print(matrix.shape)
+random_tensor=torch.rand(2,2,3,4) # this is the size of the tensor, should give the matrix 3 rows 4 columns
+print(random_tensor)
+zero=torch.zeros(3,3)
+print(zero)
+ones=torch.ones(size=(3,3)) # could put size=() argument also but it is already there bu default
+print(ones)
+range=torch.arange(0,11,1) # gives the range of numbers from 0 to 10 with the step(difference between numbers) of 1 
+print(range)
+similar=torch.ones_like(range) # gives the same shape as the range tensor
+print(similar)
